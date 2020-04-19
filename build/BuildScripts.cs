7772124ddb7bd21ca66ae65b3325283dec381139
Connor Laderer <splashy5@hotmail.com>
2020-04-19 11:05:39 -0400

    fix package for nuke

--- build/BuildScripts.cs
+++ build/BuildScripts.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Nuke.Common;
 using Nuke.Common.Execution;
@@ -10,7 +9,6 @@ using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.ReportGenerator;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
